ff335d7a7b0c72fe1f1c5cc11ce3bd13ddcf2c5c
Bektur Razzakov <brzzkv@gmail.com>
2024-02-24 19:28:30 +0600

    Application deploy try 4

---
+++ backend/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Change directory to the backend directory
+cd /Users/brzzkvgmail.com/Downloads/ESDP/tourism-platform-concept/backend
+
+# Install dependencies
+npm install
+
+# Start the Express.js server
+npm run dev
---
+++ frontend/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Change directory to the frontend directory
+cd /Users/brzzkvgmail.com/Downloads/ESDP/tourism-platform-concept/frontend
+
+# Install dependencies
+npm install
+
+# Build the Next.js app
+npm run build
