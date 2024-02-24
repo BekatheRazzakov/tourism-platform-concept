2fc60c7693940c298fe3af6f10c14a8409734e5c
Bektur Razzakov <brzzkv@gmail.com>
2024-02-24 19:34:36 +0600

    Application deploy try 6

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 CURRENT_DIR=$(pwd)
 echo CURRENT_DIR
+echo "HELLO"
 # Change directory to the frontend directory
 cd /Users/brzzkvgmail.com/Downloads/ESDP/tourism-platform-concept/frontend
 
