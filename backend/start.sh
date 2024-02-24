cdab8145e742645dda81a356a1b0886da32e6f78
Bektur Razzakov <brzzkv@gmail.com>
2024-02-24 20:06:58 +0600

    Application deploy try 8

--- backend/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Change directory to the backend directory
-cd /backend
-
-# Install dependencies
-npm install
-
-# Start the Express.js server
-npm run dev
--- frontend/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Change directory to the frontend directory
-cd /frontend
-
-# Install dependencies
-npm install
-
-# Build the Next.js app
-npm run build
