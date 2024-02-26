752be1e7a781849341618f009df4de89260669c6
Bektur Razzakov <brzzkv@gmail.com>
2024-02-26 11:14:46 +0600

    Application deploy try 15

--- test/codecept.conf.ts
+++ test/codecept.conf.ts
@@ -20,7 +20,7 @@ export const config: CodeceptJS.MainConfig = {
     I: "./steps_file",
   },
   gherkin: {
-    features: "./features/m",
+    features: "./features/*feature",
     steps: [
       "./step_definitions/mainSliderSteps.ts",
       // "./step_definitions/registrationSteps.ts",
